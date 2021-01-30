201bdfada20b5a9c762ed92bdbe830b45140b60a
Paola Ducolin <paoletta84@hotmail.com>
2021-01-31 00:59:02 +0100

    more header styling

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <>
       <header className="h-16 bg-primary flex items-center">
-        <nav className="ml-1 h-full w-6/12 sm:w-4/12 py-1 flex items-center space-x-8 font-mono">
+        <nav className="ml-1 h-full w-full py-1 flex items-center justify-between font-mono">
           <Link href="/">
             <a className="h-full">
               <img
@@ -13,17 +13,20 @@ export default function Header() {
                 src="https://github.com/pducolin.png"
               />
             </a>
-          </Link>{" "}
-          <Link href="/">
-            <a className=" text-frontSecondary hover:text-secondary focus:text-secondary">
-              Home
-            </a>
           </Link>
-          <Link href="/about">
-            <a className=" text-frontSecondary hover:text-secondary focus:text-secondary">
-              About
-            </a>
-          </Link>
+          <div className="flex flex-row flex-grow justify-self-center space-x-8 md:space-x-28 justify-center">
+            <Link href="/">
+              <a className=" text-frontSecondary hover:text-secondary focus:text-secondary">
+                Home
+              </a>
+            </Link>
+            <Link href="/about">
+              <a className=" text-frontSecondary hover:text-secondary focus:text-secondary">
+                About
+              </a>
+            </Link>
+          </div>
+          <div>Twitter | Github</div>
         </nav>
       </header>
     </>
