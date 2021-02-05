eb6b37188736db0cd3a1bd9333cba4e3fc928c68
pducolin <paola.ducolin@datadoghq.com>
2021-02-05 22:38:35 +0100

    ♿️ [Header] hover and focus interaction

--- components/Header.js
+++ components/Header.js
@@ -6,23 +6,24 @@ export const Header = () => {
     <header className="bg-primary flex items-center h-14 p-2">
       <nav className="h-full w-full flex items-center justify-between font-mono">
         <Link href="/">
-          <a className="h-full">
+          <a className="h-full outline-none border-primary border-2 rounded-full hover:opacity-70 focus:opacity-70 focus:outline-none hover:border-frontPrimary focus:border-frontPrimary box-border transition duration-500 ease-in-out">
             <img
-              className="rounded-full border-frontSecondary border-2 hover:opacity-70 focus:opacity-70 focus:outline-none h-full hover:border-frontPrimary focus:border-frontPrimary  box-border"
+              title="home"
+              className="rounded-full h-full"
               alt="poladuco"
               src="https://github.com/pducolin.png"
             />
           </a>
         </Link>
-        <div className="flex flex-row flex-grow justify-self-center space-x-8 md:space-x-28 justify-center">
+        <div className="flex flex-row flex-grow justify-self-center space-x-8 md:space-x-20 justify-center">
           <Link href="/">
-            <a className=" text-frontSecondary hover:text-frontPrimary focus:text-frontPrimary hover:underline focus:underline">
+            <a title='home' alt='home' className="outline-none text-frontSecondary hover:text-frontPrimary focus:text-frontPrimary border-b-2 border-opacity-0 border-frontPrimary hover:border-opacity-100 focus:border-opacity-100 p-1 transition duration-500 ease-in-out">
               Home
             </a>
           </Link>
           <span className="text-frontSecondary pointer-events-none">|</span>
           <Link href="/about">
-            <a className=" text-frontSecondary hover:text-frontPrimary focus:text-frontPrimary hover:underline focus:underline">
+            <a title='about' alt='about' className="outline-none text-frontSecondary hover:text-frontPrimary focus:text-frontPrimary border-b-2 border-opacity-0 border-frontPrimary hover:border-opacity-100 focus:border-opacity-100 p-1 transition duration-500 ease-in-out">
               About
             </a>
           </Link>
@@ -32,7 +33,7 @@ export const Header = () => {
             href="https://twitter.com/poladuco"
             target="_blank"
             rel="noopener noreferrer"
-            className="border-b-2 border-opacity-0 border-frontPrimary hover:border-opacity-100 p-1"
+            className="outline-none border-b-2 border-opacity-0 border-frontPrimary hover:border-opacity-100 focus:border-opacity-100 p-1 transition duration-500 ease-in-out"
           >
             <IconTwitter
               size="20px"
@@ -43,11 +44,11 @@ export const Header = () => {
             href="https://github.com/pducolin"
             target="_blank"
             rel="noopener noreferrer"
-            className="border-b-2 border-opacity-0 border-frontPrimary hover:border-opacity-100 p-1 transition duration-500 ease-in-out "
+            className="outline-none border-b-2 border-opacity-0 border-frontPrimary hover:border-opacity-100 focus:border-opacity-100 p-1 transition duration-500 ease-in-out "
           >
             <IconGithub
               size="20px"
-              className="fill-current text-frontSecondary hover:fill-current hover:text-frontPrimary focus:fill-current focus:text-frontPrimary"
+              className="fill-current text-frontSecondary hover:fill-current hover:text-frontPrimary focus:border-opacity-100 focus:fill-current focus:text-frontPrimary"
             />
           </a>
         </div>
