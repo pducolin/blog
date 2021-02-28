6c37b29a89a3715d6475ec4ec55a5e3dc1370130
Paola Ducolin <paoletta84@hotmail.com>
2021-02-28 01:04:43 +0100

    add analytics script

---
+++ scripts/analytics.js
@@ -0,0 +1,26 @@
+const MICROANALYTICS_URL = "https://microanalytics.io/api/event"
+const MICROANALYTICS_CONTENT_TYPE = "application/json, text/javascript; charset=utf-8"
+
+const postAnalytics = async () => {
+  let resolution = undefined
+  if (window && window.width && window.height) {
+    resolution = `${window.width}x${window.height}`
+  }
+  await fetch(MICROANALYTICS_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": MICROANALYTICS_CONTENT_TYPE
+    },
+    body: JSON.stringify({
+      page: window.location.href,
+      screen_resolution: resolution
+    })
+  })
+}
+
+// respect user's Do Not Track setting
+if (navigator?.doNotTrack) {
+  return
+}
+
+postAnalytics()
