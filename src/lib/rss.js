b6e08e66505af692f2901fbe97a9d5d59435ff55
pducolin <45568537+pducolin@users.noreply.github.com>
2024-02-09 00:03:46 +0100

    add base url to rss item links (#21)
    * add base url to rss item links
    
    * fix validation
    
    * fix link
    
    * remove atom link
    
    * add atom link

--- src/lib/rss.js
+++ src/lib/rss.js
@@ -10,33 +10,34 @@ export const generateRSSFeed = ({ title, url, description }) => {
 }
 
 const createRSSFeedContent = ({ title, url, description, posts }) => {
-  const { rssItems, latestPostDate } = createRSSItemsFromPosts({ posts })
+  const { rssItems, latestPostDate } = createRSSItemsFromPosts({ posts, url })
 
   return `<?xml version="1.0" ?>
-<rss version="2.0">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
+  <atom:link href="${url}/rss.xml" rel="self" type="application/rss+xml" />
       <title>${title}</title>
       <link>${url}</link>
       <description>${description}</description>
       <language>en</language>
-      <lastBuildDate>${latestPostDate.toString()}</lastBuildDate>
+      <lastBuildDate>${latestPostDate.toUTCString()}</lastBuildDate>
       ${rssItems}
   </channel>
 </rss>`
 }
 
-const createRSSItemsFromPosts = ({ posts }) => {
+const createRSSItemsFromPosts = ({ posts, url }) => {
   var rssItems = ""
-  var latestPostDate = new Date(null)
+  var latestPostDate = new Date(1970, 1, 1)
   // Add each individual post to the feed.
   posts.forEach((post) => {
     const pubDate = new Date(post.frontmatter.date)
     rssItems += `<item>
   <title>${post.frontmatter.title}</title>
-  <link>/post/${post.id}</link>
+  <link>${url}/post/${post.id}</link>
   <author>${post.frontmatter.author}</author>
-  <pubDate>${pubDate.toString()}</pubDate>
-  <guid>/post/${post.id}</guid>
+  <pubDate>${pubDate.toUTCString()}</pubDate>
+  <guid>${url}/post/${post.id}</guid>
 </item>`
 
     if (pubDate > latestPostDate) {
